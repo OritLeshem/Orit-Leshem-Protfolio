3714f3d9e86d513b90d46639a63e7d340e2575fb
OritLeshem <azmono@gmail.com>
2022-12-05 16:39:09 -0500

    Add projs-service.js, build  gProjs array, build func getProjs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 console.log('Starting up');
+
+function initPage() {
+  // renderProjs()
+}
+function renderProjs() {
+  console.log('render proj')
+  var projs = getProjs()
+  console.log(projs)
+  var strHtmls = projs.map(proj => `<div class="col-md-4 col-sm-6 portfolio-item">
+            <a
+              class="portfolio-link"
+              data-toggle="modal"
+              href="#portfolioModal1"
+            >
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img
+                class="img-fluid"
+                src="img/portfolio/01-thumbnail.jpg"
+                alt=""
+              />
+            </a>
+            <div class="portfolio-caption">
+              <h4>Threads</h4>
+              <p class="text-muted">Illustration</p>
+            </div>
+          </div>`
+
+  )
+  document.querySelector('.projs').innerHTML = strHtmls.join('')
+
+}
---
+++ js/proj-service.js
@@ -0,0 +1,69 @@
+console.log('porj-service.js')
+var gProjs
+var Lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+gProjs = [
+  {
+    id: '01-thumbnail',
+    name: 'Threads',
+    title: 'Threads',
+    desk: Lorem,
+    url: "img/portfolio/01-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+  {
+    id: '02-thumbnail',
+    name: 'Explore',
+    title: 'Explore',
+    desk: Lorem,
+    url: "img/portfolio/02-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+  {
+    id: '03-thumbnail',
+    name: 'Threads',
+    title: 'Threads',
+    desk: Lorem,
+    url: "img/portfolio/03-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+  {
+    id: '04-thumbnail',
+    name: 'Explore',
+    title: 'Explore',
+    desk: Lorem,
+    url: "img/portfolio/04-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+  {
+    id: '05-thumbnail',
+    name: 'Threads',
+    title: 'Threads',
+    desk: Lorem,
+    url: "img/portfolio/05-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+  {
+    id: '06-thumbnail',
+    name: 'Explore',
+    title: 'Explore',
+    desk: Lorem,
+    url: "img/portfolio/06-thumbnail.jpg",
+    publidhedAt: Date.now(),
+    labels: ["Matrixe", "keyboard events"],
+
+  },
+
+]
+function getProjs() {
+  return gProjs
+}
